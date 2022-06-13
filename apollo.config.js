88a263ce54574675a30db347eac1039745031ba3
Parth <parthp1808@gmail.com>
2022-06-13 13:29:11 -0400

    Using WEB_UI_API_URL in apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,9 @@
 /* eslint import/no-commonjs: off */
-
 module.exports = {
   client: {
     service: {
       name: 'bobarr',
-      url: 'http://localhost:4000/graphql',
+      url: process.env.WEB_UI_API_URL+'/graphql' || `http://${host}:4000/graphql`,
     },
     excludes: ['**/*.{ts,tsx,js,jsx}'],
   },
